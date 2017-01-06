197aa18896328b192c9ace51cccd417333aaaf50
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-05 18:18:10 -0600

    Log tags are corrected

--- lib/event_store/cluster/leader_status/log.rb
+++ lib/event_store/cluster/leader_status/log.rb
@@ -3,7 +3,8 @@ module EventStore
     module LeaderStatus
       class Log < ::Log
         def tag!(tags)
-          tags << :event_store_clustering
+          tags << :event_store_cluster_leader_status
+          tags << :event_store_cluster
           tags << :event_store
           tags << :verbose
         end
